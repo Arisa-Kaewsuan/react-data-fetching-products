c4eb1ac0017b92c52e456b1cb32b1e92fe284694
Kittiphat <kittiphat.rpp@gmail.com>
2025-03-25 14:32:09 +0700

    feat(data): implement product read and delete operations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,74 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [status, setStatus] = useState("loading");
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  const getProductData = async () => {
+    setStatus("loading");
+
+    try {
+      const response = await axios.get("http://localhost:4001/products");
+      setProducts(response.data.data);
+      setStatus("complete");
+    } catch (error) {
+      console.error("Error fetching product data:", error);
+      setStatus("failed");
+    }
+  };
+
+  const deleteProduct = async (productId) => {
+    try {
+      await axios.delete(`http://localhost:4001/products/${productId}`);
+      setProducts(products.filter((product) => product.id !== productId));
+      alert("Delete Product ID = " + productId + " !!");
+    } catch (error) {
+      console.error("Can't delete data", error);
+    }
+  };
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
-      <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
 
-          <button className="delete-button">x</button>
+      {status === "loading" && (
+        <div className="loading-message" style={{ textAlign: "center" }}>
+          Loading...
         </div>
-      </div>
+      )}
+      {status === "failed" && (
+        <div className="loading-message" style={{ textAlign: "center" }}>
+          Fetching Error...
+        </div>
+      )}
+      {products.map((product) => {
+        return (
+          <div key={product.id} className="product-list">
+            <div className="product">
+              <div className="product-preview">
+                <img src={product.image} alt="some product" width="80" height="80" />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {product.name}</h1>
+                <h2>Product price: {product.price} Baht</h2>
+                <p>Product description: {product.description}</p>
+              </div>
+
+              <button className="delete-button" onClick={() => deleteProduct(product.id)}>
+                x
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
